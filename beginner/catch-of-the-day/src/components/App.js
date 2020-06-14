cdcaf17e5a1ba9a5d3b9fd380fd876149608dd9c
AugmenTab <tbaum92@live.com>
2020-06-13 20:33:16 -0600

    Completed lesson 21.

--- beginner/catch-of-the-day/src/components/App.js
+++ beginner/catch-of-the-day/src/components/App.js
@@ -40,12 +40,22 @@ class App extends React.Component{
         fishes[key] = updatedFish;
         this.setState({ fishes });
     };
+    deleteFish = (key) => {
+        const fishes = { ...this.state.fishes };
+        fishes[key] = null;
+        this.setState({ fishes });
+    };
     loadSampleFishes = () => {
         this.setState({ fishes: sampleFishes });
     };
     addToOrder = (key) => {
         const order = { ...this.state.order };
         order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    };
+    removeFromOrder = (key) => {
+        const order = { ...this.state.order };
+        delete order[key];
         this.setState({ order });
     };
     render(){
@@ -64,10 +74,15 @@ class App extends React.Component{
                         ))}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Order 
+                    fishes={this.state.fishes} 
+                    order={this.state.order} 
+                    removeFromOrder={this.removeFromOrder}
+                />
                 <Inventory
                     addFish={this.addFish}
                     updateFish={this.updateFish}
+                    deleteFish={this.deleteFish}
                     loadSampleFishes={this.loadSampleFishes}
                     fishes={this.state.fishes}
                 />
