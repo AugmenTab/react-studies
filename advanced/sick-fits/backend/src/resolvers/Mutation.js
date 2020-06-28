b05511acfc2ffa007689dbf83ebe7a3a1efd5065
AugmenTab <tbaum92@live.com>
2020-06-27 18:50:09 -0600

    Completed lesson 31.

--- advanced/sick-fits/backend/src/resolvers/Mutation.js
+++ advanced/sick-fits/backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
@@ -84,6 +85,12 @@ const Mutations = {
         const res = await ctx.db.mutation.updateUser({
             where: { email: args.email },
             data: { resetToken, resetTokenExpiry },
+        });
+        const mailRes = await transport.sendMail({
+            from: 'noreply@sickfits.com',
+            to: user.email,
+            subject: 'Your Password Reset Token',
+            html: makeANiceEmail(`Your Password Reset Token is here! \n\n <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Reset Password</a>`),
         });
         return { message: 'Thanks!' };
     },
