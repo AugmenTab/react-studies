2f8127f64e6c1f9e9ae05e4cb0f181c5501c6666
AugmenTab <tbaum92@live.com>
2020-06-28 12:28:33 -0600

    Completed lesson 34.

--- advanced/sick-fits/backend/src/index.js
+++ advanced/sick-fits/backend/src/index.js
@@ -16,7 +16,15 @@ server.express.use((req, res, next) => {
     next();
 });
 
-// TODO Use express middleware to populate current user
+server.express.use(async (req, res, next) => {
+    if(!req.userId) return next();
+    const user = await db.query.user(
+        { where: { id: req.userId } },
+        '{ id, permissions, email, name }'
+    );
+    req.user = user;
+    next();
+});
 
 server.start({
     cors: {
