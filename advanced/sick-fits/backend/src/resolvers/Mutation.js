b7ab9e6d58c87c0480079983a7e4ff44d11fb637
AugmenTab <tbaum92@live.com>
2020-08-07 14:25:44 -0600

    Completed lesson 42.

--- advanced/sick-fits/backend/src/resolvers/Mutation.js
+++ advanced/sick-fits/backend/src/resolvers/Mutation.js
@@ -190,6 +190,26 @@ const Mutations = {
             },
         }, info);
     },
+    async removeFromCart(parent, args, ctx, info) {
+        const cartItem = await ctx.db.query.cartItem(
+            {
+                where: {
+                    id: args.id,
+                },
+            },
+            `{ id, user { id }}`
+        );
+        if(!cartItem) throw new Error('No Cart Item Found!');
+        if (cartItem.user.id !== ctx.request.userId) {
+            throw new Error('This is not your cart item!');
+        };
+        return ctx.db.mutation.deleteCartItem(
+            {
+                where: { id: args.id },
+            }, 
+            info
+        );
+    },
 };
 
 module.exports = Mutations;
